4bdfc3693d43a5ceb8c8a5f1b4bb2b6a4856da56
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-19 10:22:42 +0300

    setting up file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchFilter from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchFilter />
     </div>
   );
 }
